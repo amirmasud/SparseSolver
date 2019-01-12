0d904fe709a24ed4bc3c0800cd31b1569b4730c3
Amir Masud Zare Bidaki <zare.masud@gmail.com>
2019-01-12 03:43:39 +0330

    get matrices path from argv, use -O3 for optimal compilation

--- main.cpp
+++ main.cpp
@@ -6,18 +6,25 @@
 #include "SupernodalTSolver.h"
 
 
-int main() {
+int main(int argc, char *argv[]) {
+    if(argc < 2){
+        std::cout<<"The input matrices is missing!"<<std::endl;
+        std::cout<<"First input is the path of  L matrix and the second is for b matrix!"<<std::endl;
+        return -1;
+    }
+    std::string L_fileName = argv[1];
+    std::string B_fileName = argv[2];
 
-//    const auto *L = utils::CSCMatrix::createFromFile("af_0_k101.mtx");
-    const auto *b = utils::CSCVector::createFromFile("b_sparse_af_0_k101.mtx");
-//    auto naiveTSolver = NaiveTSolver::create(L, b);
-//    naiveTSolver->solve();
-//    naiveTSolver->evaluate();
+    const auto *L1 = utils::CSCMatrix::createFromFile(L_fileName);
+    const auto *b = utils::CSCVector::createFromFile(B_fileName);
+    auto naiveTSolver = NaiveTSolver::create(L1, b);
+    naiveTSolver->solve();
+    naiveTSolver->evaluate();
 //    const auto *newL = utils::AugmentedCSCMatrix::createFromFile("af_0_k101.mtx");
 //    newL->print();
 
-    const auto *L = utils::SupernodalCSCMatrix::createFromFile("af_0_k101.mtx");
-    auto supernodalTSovler = SupernodalTSolver::create(L,b);
+    const auto *L2 = utils::SupernodalCSCMatrix::createFromFile(L_fileName);
+    auto supernodalTSovler = SupernodalTSolver::create(L2,b);
     supernodalTSovler->solve();
     supernodalTSovler->evaluate();
 //    auto decoupledTSolver = DecoupledTSolver::create(L, b);
